1aa97b0a79e7106b9cb2e2bf3f5b0cb452355ab7
MANAGAR <rasmussjak@gmail.com>
2024-03-22 13:12:07 +0100

    Generalized enemy behaviour(WIP)

--- Unity Project/Dungun 2/Assets/Scripts/GameMechanics/EnemyBehavior.cs
+++ Unity Project/Dungun 2/Assets/Scripts/GameMechanics/EnemyBehavior.cs
@@ -10,20 +10,34 @@ public class EnemyBehavior : MonoBehaviour
     [SerializeField] NavMeshAgent agent;
     [SerializeField] GameObject bulletPrefab;
 
+    public enum MovementMode
+    {
+        Idle,
+        Waiting,
+        Wandering,
+        Chasing
+    }
+
     /*Cooldowns and timers*/
-    public float shootCooldownTime = 1f;  //1 second
+    public float shootCooldownTime = 1f;  //rasj: 1 second
     private float shootCooldown = 0f;
 
     public float minTargetDistance = 3f;
     public float maxTargetDistance = 10f;
 
-    private Vector3 wandDesti = new Vector3 (0f, 0f, 0f);
+    private Vector3 wandDesti = new Vector3(0f, 0f, 0f);
     public float maxWanderDistance = 5f;
     public float maxWanderTime = 15f;
     private float wanderTime = 0f;
     private bool wandering = false;
 
+    private MovementMode MMode = MovementMode.Idle;
+
+    //Gameobject enemy = gameobj.GetComponent<Enemy>();
+
     public int healthPoints = 3;
+
+    public int sstate = 0;
 
     void Start()
     {
@@ -54,31 +68,68 @@ public class EnemyBehavior : MonoBehaviour
     void Move(Vector3 targetPos, float minDist, float maxDist)
     {
         wandering = false;
-        float dist = Vector3.Distance(targetPos, transform.position);  //rasj: Get distance
 
-        if (dist > minDist && dist < maxDist)  //rasj: if enemy is far enough away from target
+        if (MMode == MovementMode.Chasing)  //rasj: if enemy is far enough away from target  dist > minDist && dist < maxDist
         {
             agent.SetDestination(targetPos);  //rasj: Sets destination to target
+            Flip(targetPos);
         }
-        else if (dist > maxDist)  //rasj: if enemy is too far away from target
+        else if (MMode == MovementMode.Idle || MMode == MovementMode.Wandering)  //rasj: if enemy is too far away from target  dist > maxDist
         {
             wandering = true;
             Wander(-maxWanderDistance, maxWanderDistance);
         }
-        else  //rasj: if enemy is too close to target
+        else if (MMode == MovementMode.Waiting)  //rasj: if enemy is too close to target
         {
             agent.SetDestination(transform.position);  //rasj: Stops enemy from going further
         }
+
     }
 
     void Wander(float min, float max)
     {
         if (wandDesti == transform.position || wanderTime >= maxWanderTime)  //rasj: if wander reached or too much time has passed
         {
-            wanderTime = 0;
-            Vector3 randVect = new Vector2(Random.Range(min, max), Random.Range(min, max));
-            wandDesti = randVect + transform.position;
-            agent.SetDestination(wandDesti);
+            wanderTime = 0;  //rasj: Reset the time used while wandering
+            Vector3 randVect = new Vector2(Random.Range(min, max), Random.Range(min, max));  //rasj: get a random vector
+            wandDesti = randVect + transform.position;  //rasj: Add that vector to the currect position
+            agent.SetDestination(wandDesti);  //rasj: Set that to the new desination
+
+            Flip(wandDesti); //rasj: flip the sprite accordingly
+        }
+    }
+
+    public void ChangeBehaviour(MovementMode newMovmentMode)
+    {
+        MMode = newMovmentMode;
+
+        /*
+        float dist = Vector3.Distance(target.position, transform.position);  //rasj: Get distance
+        if (dist > minDist && dist < maxDist)  //rasj: if not too far away and not too close
+        {
+            MMode = MovementMode.Chasing;
+        }
+        else if (dist > maxDist)  //if too far away
+        {
+            MMode = MovementMode.Idle;
+        }
+        else if (dist < minDist)
+        {
+            MMode = MovementMode.Wandering;
+        }
+        */
+    }
+
+    void Flip(Vector3 targetPos)
+    {
+        float dirX = targetPos.x - transform.position.x;
+        if (dirX < 0)  //rasj: left
+        {
+            transform.rotation = Quaternion.Euler(0, 180f, 0);
+        }
+        else if (dirX > 0) //rasj: right
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
         }
     }
 
@@ -91,26 +142,31 @@ public class EnemyBehavior : MonoBehaviour
             shootCooldown = 0f;  //rasj: reset cooldown
             GameObject newBullet = Instantiate(bulletPrefab);
             newBullet.transform.up = dir;
-            newBullet.transform.position = transform.position;  //rasj: set to current position
+            newBullet.transform.position = transform.position + dir;  //rasj: set to current position
         }
     }
 
     Vector2 PointTo(Vector2 targetPos)  //rasj: Sets rotation
     {
-        //TODO: make enemy point ahead of player, to actually hit
+        //TODO: make enemy point ahead of player's direction, to compensate for time and actually hit
         //rasj: findes the vector to the target
         Vector2 dir = new Vector2(targetPos.x - transform.position.x, targetPos.y - transform.position.y);
         //transform.up = dir;
         return dir;
     }
 
-    void TakeDamage(int damage)  //TODO: run this when collide with player bullet
+    void TakeDamage(int damage)
     {
         healthPoints -= damage;
 
         if (healthPoints <= 0)
         {
-            Destroy(this.gameObject);
+            Death();
         }
+    }
+
+    void Death()
+    {
+        Destroy(this.gameObject);
     }
 }
---
+++ Unity Project/Dungun 2/Assets/Scripts/GameMechanics/Instruction.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
+using static UnityEngine.GraphicsBuffer;
+
+namespace Assets.Scripts.GameMechanics
+{
+    [Serializable]
+    abstract public class Instruction
+    {
+        abstract public IEnumerator ExecuteInstruction();
+        public InstructionType type;
+    }
+
+    [Serializable]
+    public class WaitInstruction : Instruction
+    {
+        public float time = 0f;
+        override public IEnumerator ExecuteInstruction()
+        {
+            yield return new WaitForSeconds(time);
+        }
+    }
+
+    [Serializable]
+    public class RotateInstructon : Instruction
+    {
+        [SerializeField] Transform enemy;
+        public float angle = 0;
+
+        public override IEnumerator ExecuteInstruction()
+        {
+            enemy.transform.RotateAround(enemy.transform.position, new Vector3 (0, 0, 1), angle);
+            yield return null;  //rasj: just stop
+        }
+    }
+
+    [Serializable]
+    public class PointInstruction : Instruction
+    {
+        [SerializeField] Transform enemy;
+        [SerializeField] Transform target;
+
+        public override IEnumerator ExecuteInstruction()
+        {
+            enemy.transform.up = target.position - enemy.position;
+            yield return null;
+        }
+    }
+
+    [Serializable]
+    public class DieInstruction : Instruction
+    {
+        [SerializeField] Transform enemy;
+
+        public override IEnumerator ExecuteInstruction()
+        {
+            GameObject.Destroy(enemy.gameObject);
+            yield return null;
+        }
+    }
+
+    [Serializable]
+    public class ShootInstruction : Instruction
+    {
+        [SerializeField] Transform enemy;
+        [SerializeField] GameObject bulletPrefab;
+
+        public override IEnumerator ExecuteInstruction()
+        {
+            GameObject newBullet = GameObject.Instantiate(bulletPrefab);
+            newBullet.transform.up = enemy.transform.up;
+            newBullet.transform.position = enemy.transform.position + newBullet.transform.up;
+            yield return null;
+        }
+    }
+
+    [Serializable]
+    public class ChangeBehaviourtInstruction : Instruction
+    {
+        [SerializeField] Transform enemy;
+        [SerializeField] Transform target;
+        public ComparisonMode comparisonMode;
+        public EnemyBehavior script;
+        public EnemyBehavior.MovementMode newMovementMode;
+        public float distToPlayer;
+
+        public override IEnumerator ExecuteInstruction()
+        {
+            float dist = Vector3.Distance(enemy.transform.position, target.transform.position);
+            bool condition = false;
+            switch (comparisonMode)
+            {
+                case ComparisonMode.Less:
+                    if (distToPlayer < dist) { condition = true; }
+                    break;
+                case ComparisonMode.Greater:
+                    if (distToPlayer > dist) { condition = true; }
+                    break;
+            }
+            if (condition)
+            {
+                script.ChangeBehaviour(newMovementMode);
+            }
+            yield return null;
+        }
+    }
+
+    public enum ComparisonMode
+    {
+        None,
+        Greater,
+        Less
+    }
+
+    public enum InstructionType
+    {
+        None,
+        Wait,
+        Rotate,
+        ChangeBehaviour,
+        Point,
+        Shoot,
+        Die
+    }
+}
